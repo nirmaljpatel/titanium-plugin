a0ce201e6795db768d022b28571cf6a620d78a9f
Nirmal Patel <nirmaljpatel@gmail.com>
2016-12-20 21:28:55 -0600

    Update tealium.js

--- Samples/Alloy/app/assets/tealium.js
+++ Samples/Alloy/app/assets/tealium.js
@@ -1,20 +1,20 @@
-Tealium = {};
+var Tealium = {};
 
 if (Ti.Platform.name == "android") {
 	Tealium = require('com.tealium.appcelerator.android');
 } else if (Ti.Platform.name == "iPhone OS") {
 	Tealium = require('com.tealium.appcelerator.ios');
-	Tealium.initialize = function(accountName, profileName, environment, isRelease) {
+	exports.initialize = function(accountName, profileName, environment, isRelease) {
 		Tealium.enableWithOptions(accountName, profileName, environment, isRelease);
 	};
-	Tealium.trackView = function(viewName, data) {
+	exports.trackView = function(viewName, data) {
 		data = data || {};
 		if(viewName) {
 			data.screen_title = viewName;	
 		}
 		Tealium.trackViewWithData(data);
 	};
-	Tealium.trackEvent = function(eventName, data) {
+	exports.trackEvent = function(eventName, data) {
 		data = data || {};
 		if(eventName) {
 			data.link_id = eventName;
@@ -22,13 +22,13 @@ if (Ti.Platform.name == "android") {
 		Tealium.trackEventWithData(data);
 	};
 } else {
-	Tealium.initialize = function(accountName, profileName, environment, isRelease) {
+	exports.initialize = function(accountName, profileName, environment, isRelease) {
 		Ti.API.warn("Tealium plugin does not support " + Ti.Platform.name);
 	};
-	Tealium.trackView = function(viewName, data) {
+	exports.trackView = function(viewName, data) {
 		Ti.API.warn("Tealium plugin does not support " + Ti.Platform.name);
 	};
-	Tealium.trackEvent = function(eventName, data) {
+	exports.trackEvent = function(eventName, data) {
 		Ti.API.warn("Tealium plugin does not support " + Ti.Platform.name);
 	};
 }
